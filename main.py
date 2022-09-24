d9e36c498713465179d85f3abcc5280ec72ec415
Dr Dre <dr.dre@dyn172-30-100-38.wireless.uwo.pri>
2022-09-23 23:21:47 -0400

    Test run 1
    Figuring out the basics of pandas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-melbourne_file_path = '/Users/dr.dre/Downloads/melb_data.csv'
-melbourne_data = pd.read_csv(melbourne_file_path) 
-
-melbourne_data.shape()
+pokemon_data = 'pokemon_data.csv'
+poke = pd.read_csv(pokemon_data)
+ 
+print(poke.Name)
