d0a97f02eb6b26234856e94c790f8f6b4bce1733
Dr Dre <dr.dre@dyn172-30-100-38.wireless.uwo.pri>
2022-09-23 23:37:58 -0400

    Describing specific actions in pandas

--- main.py
+++ main.py
@@ -3,4 +3,19 @@ import pandas as pd
 pokemon_data = 'pokemon_data.csv'
 poke = pd.read_csv(pokemon_data)
  
-print(poke.Name)
+# PRINT SPECIFIC COLUMNS:
+#   print(poke[['Name', 'Type 1', 'HP']])
+
+# PRINT A SPECIFIC ITEM BASED ON ITS INDEX:
+#   print(poke.iloc[2,1])
+
+# PRINT OUT ALL ROWS:
+# for index, row in poke.iterrows():
+ #    print(index, row['Name'])
+
+# PRINT OUT ALL WATER TYPE POKEMON ROWS:
+# waterTypes = poke.loc[poke['Type 1'] == 'Water']
+# print(waterTypes)
+
+# GET IN DEPTH STATS ON EVERY ROW AND COLUMN:
+# poke.describe()
