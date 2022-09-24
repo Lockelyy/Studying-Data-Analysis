9e3128d787a8c1bf56aef1ac9e9c2bf499fe2b2e
Dr Dre <dr.dre@dyn172-30-100-38.wireless.uwo.pri>
2022-09-24 14:54:54 -0400

    Finishing up basic pandas commands

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ pokemon_data = 'pokemon_data.csv'
 poke = pd.read_csv(pokemon_data)
  
 # PRINT SPECIFIC COLUMNS:
-#   print(poke[['Name', 'Type 1', 'HP']])
+    #   print(poke[['Name', 'Type 1', 'HP']])
 
 # PRINT A SPECIFIC ITEM BASED ON ITS INDEX:
-#   print(poke.iloc[2,1])
+    #   print(poke.iloc[2,1])
 
 # PRINT OUT ALL ROWS:
-# for index, row in poke.iterrows():
- #    print(index, row['Name'])
+    # for index, row in poke.iterrows():
+     #    print(index, row['Name'])
 
 # PRINT OUT ALL WATER TYPE POKEMON ROWS:
-# waterTypes = poke.loc[poke['Type 1'] == 'Water']
-# print(waterTypes)
+    # waterTypes = poke.loc[poke['Type 1'] == 'Water']
+    # print(waterTypes)
 
 # GET IN DEPTH STATS ON EVERY ROW AND COLUMN:
-# poke.describe()
+    # poke.describe()
+'''
+alphabeticalPoke = poke.sort_values('Name')
+for index, row, in poke.iterrows():
+    print(alphabeticalPoke['Name'])
+'''
+
+# ADDING NEW COLUMN BY ADDING VALUES OF OTHER COLUMNS:
+    # poke['Combat Stats'] = poke['Attack']+ poke['Sp. Atk'] + poke['Sp. Def']
+    # print(poke.head(5))
