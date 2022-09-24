87a75b5548ab94bf580dbf4bdd71b261b1ff3b3d
Dr Dre <dr.dre@dyn172-30-100-38.wireless.uwo.pri>
2022-09-24 19:30:29 -0400

    Latest: no more bag columns and sorted plus set 'Date' as index

--- Python  Files/avocadoz.py
+++ Python  Files/avocadoz.py
@@ -1,18 +1,28 @@
-import pandas as pd
+import pandas as pd # Uses C++ btw 
 import quandl
 import matplotlib
 import time
 
 avocado_data = 'Datasets/avocado.csv'
 df = pd.read_csv(avocado_data)
+df = df.copy()[df['type']=='organic']
 
-sorted_price = (df['AveragePrice']).sort_values()
-price_by_year = (df[['year', 'AveragePrice']])
+# Sorting and setting index to 'Date'
+df.sort_values(by='Date', ascending=True, inplace = True)
+df.set_index('Date', inplace=True)
+
+# Get rid of bag columns, bag bad
+del df['Small Bags'], df['Large Bags'], df['Total Bags'], df['XLarge Bags']
+
+print(df.head())
+
+# Working on it...
+
+price_by_year = (df[['year', 'AveragePrice']]).sort_values(by='AveragePrice', ascending=True)
 print(price_by_year.head(5), "\n")
 
-
+# Prints out every single row that has 'Albany' as the region. With 'Date' as the index.
 df_albany = df[ df['region'] == 'Albany' ]
-df_albany.set_index('Date', inplace = True)
 print(df_albany.head(), "\n")
 
 # Prints out every unique value from the specified column
@@ -20,16 +30,5 @@ print(df['region'].unique())
 
 
 
-'''
 
-df = quandl.get('WIKI/GOOGL')
-
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-df['High Low Percent'] = round((df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0, 2)
-df['Percent change in stock'] = round((df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0, 2)
-
-df = df[['Adj. Close', 'High Low Percent', 'Percent change in stock', 'Adj. Volume']]
-print(df.head(5))
-
-'''
 
